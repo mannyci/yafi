ff4e12c4b98ea82282773fd699f99974c39dd5f3
mannyci <manasheart@gmail.com>
2018-08-20 07:32:27 +0200

    Rebase

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 import sys
 import datetime
 import platform
-# import logging
+import ConfigParser
+from logging.handlers import RotatingFileHandler
+import logging
 from flask import Flask, Response, redirect, url_for, request, session, abort, render_template, flash, jsonify
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 
@@ -13,9 +15,55 @@ from views.admin.view import admin
 from views.jobs.view import jobs
 
 # # Initialize database
-# import models.database
-# import views
-# db = models.database.DB()
+import models.database
+db = models.database.DB()
+
+defaults = {
+    u'host': u'manny.com',
+    u'port': u'5000',
+    u'db_host': u'0.0.0.0',
+    u'loglevel': u'info',
+    u'logfile': u'var/app.log',
+}
+
+class conf(object):
+    """docstring for conf"""
+    # def __init__(self):
+    #     self.read_config()
+    def read_config(self):
+        # Read configuration from config file
+        self.default_conf = os.path.join('etc', 'app.cfg')
+        cp = ConfigParser.ConfigParser(defaults)
+        cp.read(self.default_conf)
+        self.config = cp
+        try:
+            self.host = self.config.get('app', 'host')
+            self.port = self.config.get('app', 'port')
+        except Exception as e:
+            print "Config error:",e
+            sys.exit(1)
+
+class log(object):
+
+    def start_log(self):
+        self.default_conf = os.path.join('etc', 'app.cfg')
+        cp = ConfigParser.ConfigParser(defaults)
+        cp.read(self.default_conf)
+        self.config = cp
+        self.logfile = self.config.get('log', 'logfile')
+        self.loglevel = self.config.get('log', 'loglevel')
+        """Configure the default logging module"""
+        handler = []
+        # Add the log message handler to the logger
+        handler.append(RotatingFileHandler(
+                      self.logfile, maxBytes=10 * 1024 * 1024, backupCount=4))
+        handler.append(logging.StreamHandler())
+        # Set up a specific logger with our desired output level
+        self.logger = logging.getLogger()
+        self.logger.setLevel(self.loglevel.upper())
+        for h in handler:
+            h.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+            self.logger.addHandler(h)
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.secret_key = os.urandom(24)
@@ -65,4 +113,6 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
+    conf().read_config()
+    log().start_log()
     app.run(debug=True)
