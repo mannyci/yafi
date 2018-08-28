fbb5091d73c2e29c16d3424a2a2800b06410ce05
Manas Maiti <39604499+mannyci@users.noreply.github.com>
2018-08-28 15:17:26 +0200

    Sqlalchemy models

--- views/admin/view.py
+++ views/admin/view.py
@@ -1,29 +1,30 @@
 from flask import Blueprint
 from flask import Flask, Response, redirect, url_for, request, session, abort, render_template, flash, jsonify
 from flask_login import LoginManager, UserMixin, login_required
-from models.database import db
+from models.database import db, Users
 
 admin = Blueprint('admin', __name__)
 
 @admin.route('/user/<username>', methods=['GET', 'POST'])
 @login_required
 def user_profile(username):
+    print username
     if request.method == 'GET':
-        return render_template("user.html", data=db.get_user_profile(username))
+        return render_template("user.html", user=Users.query.filter_by(username=username).first())
 
 @admin.route('/user/<username>/edit', methods=['GET', 'POST'])
 @login_required
 def edit(username):
     if request.method == 'GET':
-        return render_template("user_edit.html", user_data=db.get_user_profile(username), dept=db.get_dept())
+        return render_template("user_edit.html", user=Users.query.filter_by(username=username).first())
     else:
-        return redirect(url_for('user_profile', username=username))
+        return redirect(url_for('admin.user_profile', username=username))
 
 @admin.route('/users', methods=['GET', 'POST'])
 @login_required
 def users():
     if request.method == 'GET':
-        return render_template("users.html", data=db.get_users())
+        return render_template("users.html", data=Users.query.all())
     else:
         print request.form
         return redirect(url_for('user_profile', username=username))
